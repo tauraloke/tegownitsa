3d8de7c572799b4b1b24da4d201557b889fbddb5
tauraloke <tauraloke@gmail.com>
2022-09-14 11:55:55 +0300

    optimize sankaku load

--- src/services/parsers/sankaku_parser.js
+++ src/services/parsers/sankaku_parser.js
@@ -1,7 +1,36 @@
-import BasicHTMLMoebooruParser from './basic_html_moebooru_parser.js';
+import BasicJsonMoebooruParser from './basic_json_moebooru_parser.js';
 
-export default class SankakuParser extends BasicHTMLMoebooruParser {
-  getClassTemplate() {
-    return '.tag-type-%namespace% > a';
+export default class SankakuParser extends BasicJsonMoebooruParser {
+  /**
+   * @returns {string}
+   */
+  getFetchUrl() {
+    return `https://capi-v2.sankakucomplex.com/posts/${this.getItemId()}`;
+  }
+  async extractTags() {
+    try {
+      let json = JSON.parse(await this.getBuffer());
+      let tags = [];
+      const prefixes = {
+        0: '',
+        5: 'genre:',
+        4: 'character:',
+        3: 'series:',
+        1: 'creator:',
+        8: 'meta:'
+      };
+      if (!json.tags) {
+        return [];
+      }
+      for (let i in json.tags) {
+        let tag = json.tags[i];
+        let prefix = prefixes[tag.type] || '';
+        tags.push(prefix + tag.tagName);
+      }
+      return tags;
+    } catch (error) {
+      console.log('Cannot parse url', this.url, error);
+      return [];
+    }
   }
 }
