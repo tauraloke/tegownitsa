e00a099e069eada895213174629caf70cb2e0622
tauraloke <tauraloke@gmail.com>
2022-09-23 21:21:32 +0300

    fix constructor

--- src/api/network/extract_data_from_source.js
+++ src/api/network/extract_data_from_source.js
@@ -4,6 +4,8 @@ import AbstractBasicParser from '@/services/parsers/abstract_basic_parser.js';
 export async function run(_event, _db, url, snake_source_key) {
   /** @type {AbstractBasicParser} */
   let source =
-    new (require(`../../services/parsers/${snake_source_key}_parser.js`))(url);
+    new (require(`../../services/parsers/${snake_source_key}_parser.js`).default)(
+      url
+    );
   return await source.parse();
 }
