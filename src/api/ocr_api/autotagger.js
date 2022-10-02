dad58b3660fc9cf029657c76902fc5ede4282f08
tauraloke <tauraloke@gmail.com>
2022-10-02 19:17:03 +0300

    fix autotagger for webp

--- src/api/ocr_api/autotagger.js
+++ src/api/ocr_api/autotagger.js
@@ -5,6 +5,7 @@ import tags from '../../config/danbooru_tags.json';
 import namespaces from '../../config/tag_namespaces.js';
 import { getItem } from '../../services/download_item_storage.js';
 import { app } from 'electron';
+import sharp from 'sharp';
 
 const LIMIT = 50;
 const PREPARED_IMAGE_SIZE = 224;
@@ -68,11 +69,22 @@ export async function run(_event, _db, filepath) {
     'serving_default'
   );
 
+  let sharpedImage = sharp(filepath);
+  let format = (await sharpedImage.metadata()).format;
+  if (!['bmp', 'jpeg', 'png', 'gif'].includes(format)) {
+    sharpedImage = sharpedImage.toFormat('jpeg').jpeg({
+      quality: 100,
+      chromaSubsampling: '4:4:4',
+      force: true
+    });
+  }
+  let buffer = await sharpedImage.toBuffer();
+
   return {
     status: 'OK',
     result: tf.tidy(() => {
       let tensor = tf.node
-        .decodeImage(fs.readFileSync(filepath), 3)
+        .decodeImage(buffer, 3)
         .resizeBilinear([PREPARED_IMAGE_SIZE, PREPARED_IMAGE_SIZE]);
       tensor = tensor.div(MAX_COLOR_VALUE);
       tensor = tensor.expandDims().transpose([0, 3, 1, 2]); // move color channel to 2nd place
