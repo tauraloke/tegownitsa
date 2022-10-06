7d9790d045e71265205b80d05848d93a9e3f2e72
tauraloke <tauraloke@gmail.com>
2022-10-06 20:16:52 +0300

    a-z

--- src/config/resource_regenerators.js
+++ src/config/resource_regenerators.js
@@ -1,5 +1,5 @@
 module.exports = [
   { mask: /cdn\.donmai\.us/, name: 'danbooru' },
   { mask: /files\.yande\.re/, name: 'yandere' },
-  { mask: /[a-z]\.sankakucomplex\.com/, name: 'sankaku' }
+  { mask: /[^a-z][a-z]\.sankakucomplex\.com/, name: 'sankaku' }
 ];
