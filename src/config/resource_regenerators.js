25b51a06ffd8beb71a53a17f75f3e1dce351bd51
tauraloke <tauraloke@gmail.com>
2022-10-07 14:34:40 +0300

    add gelbooru url regenerator

--- src/config/resource_regenerators.js
+++ src/config/resource_regenerators.js
@@ -4,5 +4,6 @@ module.exports = [
   { mask: /[^a-z][a-z]\.sankakucomplex\.com/, name: 'sankaku' },
   { mask: /(s[0-9]|static)\.zerochan\.net/, name: 'zerochan' },
   { mask: /pximg\.net/, name: 'pixiv' },
-  { mask: /konachan\.com/, name: 'konachan' }
+  { mask: /konachan\.com/, name: 'konachan' },
+  { mask: /gelbooru\.com/, name: 'gelbooru' }
 ];
---
+++ src/services/regenerators/gelbooru_regenerator.js
@@ -0,0 +1,52 @@
+import AbstractRegenerator from './abstract_regenerator.js';
+import fetch from 'node-fetch';
+import { applicationUserAgent } from '../utils.js';
+import { load } from 'cheerio';
+
+export default class KonachanRegenerator extends AbstractRegenerator {
+  /**
+   * @returns {string}
+   */
+  getMD5() {
+    let md5 = this.url.toLowerCase().match(/([0-9a-f]{32})\./)[1];
+    if (!md5) {
+      throw { message: 'cannot parse url', url: this.url };
+    }
+    return md5;
+  }
+  /**
+   * @returns {CheerioAPI}
+   */
+  async getHtmlParser() {
+    this._htmlParser = this._htmlParser || load(await this.getBuffer());
+    return this._htmlParser;
+  }
+  /**
+   * @returns {string}
+   */
+  async getBuffer() {
+    this._buffer =
+      this._buffer ||
+      (await (
+        await fetch(
+          `https://gelbooru.com/index.php?page=post&s=list&tags=md5%3A${this.getMD5()}`,
+          {
+            headers: { 'User-Agent': applicationUserAgent() },
+            method: 'GET'
+          }
+        )
+      ).text());
+    return this._buffer;
+  }
+  /**
+   * @returns {Promise<string>}
+   */
+  async regenerateUrl() {
+    let $ = await this.getHtmlParser();
+    console.log($('#container > main > div.thumbnail-container > article')); // TODO: remove
+    let id = $('#container > main > div.thumbnail-container > article')[0]
+      .attr('id')
+      .match(/p([0-9]+)/)[1];
+    return `https://konachan.com/post/show/${id}`;
+  }
+}
